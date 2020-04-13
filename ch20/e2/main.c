7c2beca7411d9a3e46d2da1a251ccf21b59ac86b
Andrew McKinlay <mckinlay.andrew@gmail.com>
2020-04-13 07:36:13 +0000

    e20-2: first solution
    e20-2 shows that SIG_IGN prevents delivery of a signal (in this case
    SIGUSR1) by successfully terminating after killing itself with an
    ignored signal.

--- ch20/e2/main.c
+++ ch20/e2/main.c
@@ -1,14 +1,17 @@
-#include <sigaction.h> // this may not be right
-#include <sigatomic.h>
+#include <signal.h>
+#include <stdio.h>
 
 int main(void) {
-  int ret;
-	// sigaction
   // TODO: can the name of act be sigaction (can an object share a type's name)?
   struct sigaction act;
-  act.sa_handler = // here I realize my plan to install handler fails because the exercise requires SIG_IGN, which uninstalls handler
-  ret = sigaction(SIG_USR1, act, NULL);
-	// kill self
-	// test flag
-	// exit_success or exit_failure
+  // TODO: does act need to be zeroed?
+  act.sa_handler = SIG_IGN;
+  sigaction(SIGUSR1, &act, NULL);
+  // TODO: inform user exit code to expect if signal actually delivered
+  kill(0, SIGUSR1);
+  // if process not abnormally terminated by now, it ignored the signal
+  fprintf(stderr, "process ignored self-sent signal\n");
+  // TODO: how do you ensure compiler supports c11 features in code? e.g., implicit returns and explicit returns without specifying a return value gauarantees exit(0)
+  // is implicit return, or explicit return without return value, from main undefined behavior in c89? the book does not seem to indicate whether exit is called at all
+  return 0;
 }
