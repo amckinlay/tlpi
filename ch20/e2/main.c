8dae173587e3bb81533839da73cac434ab1a69ed
Andrew McKinlay <mckinlay.andrew@gmail.com>
2020-04-14 03:25:44 +0000

    e20-2: cleanup, improvements
    Remove TODOs. Properly initialize struct sigaction. Print signal number
    so the user knows what exit code to expect if the signal kills process
    (128 + signal number). Remove unnecessary return since compiling with
    c11.

--- ch20/e2/main.c
+++ ch20/e2/main.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 int main(void) {
-  // TODO: can the name of act be sigaction (can an object share a type's name)?
   struct sigaction act;
-  // TODO: does act need to be zeroed?
+
   act.sa_handler = SIG_IGN;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
   sigaction(SIGUSR1, &act, NULL);
-  // TODO: inform user exit code to expect if signal actually delivered
-  kill(0, SIGUSR1);
-  // if process not abnormally terminated by now, it ignored the signal
-  fprintf(stderr, "process ignored self-sent signal\n");
-  // TODO: how do you ensure compiler supports c11 features in code? e.g., implicit returns and explicit returns without specifying a return value gauarantees exit(0)
-  // is implicit return, or explicit return without return value, from main undefined behavior in c89? the book does not seem to indicate whether exit is called at all
-  return 0;
+ 
+  fprintf(stderr, "killing self with SIGUSR1 (signal number %d)\n", SIGUSR1);
+  kill(0, SIGUSR1); // if process does not terminate here it survived signal
+ 
+  fprintf(stderr, "process survived self-kill\n");
 }
