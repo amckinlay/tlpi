bd784a4acdbb3eeb45564aff949d522871d0005c
Andrew McKinlay <mckinlay.andrew@gmail.com>
2020-04-17 08:02:32 +0000

    l20-6: attempt without error checking

---
+++ ch20/l6/main.c
@@ -0,0 +1,23 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  pid_t pid;
+  int signum;
+
+  if (argc != 4 && argc != 5) {
+    printf("expected 3 or 4 arguments, got %d\n", argc);
+    exit(EXIT_FAILURE);
+  }
+
+  pid = atoi(argv[1]);
+  signum = atoi(argv[3]);
+  for (int n = atoi(argv[2]); n > 0; --n) {
+    kill(pid, signum);
+  }
+
+  if (argc == 5) {
+    kill(pid, atoi(argv[4]));
+  }
+}
